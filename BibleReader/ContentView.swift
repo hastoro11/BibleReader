9fe05a404451b7a5b9db3da3169fd4bffcd86057
Gabor Sornyei <info@sornyei.com>
2020-02-04 19:16:01 +0100

    Initial Commit

---
+++ BibleReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BibleReader
+//
+//  Created by Gabor Sornyei on 2020. 02. 04..
+//  Copyright © 2020. Gabor Sornyei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
