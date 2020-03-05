1a154f7343f613b41ec590786adcc582951f5d89
Gabor Sornyei <info@sornyei.com>
2020-03-05 11:16:35 +0100

    ChapterView - refactored

---
+++ BibleReader/Constants/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  BibleReader
+//
+//  Created by Gabor Sornyei on 2020. 03. 05..
+//  Copyright © 2020. Gabor Sornyei. All rights reserved.
+//
+
+import Foundation
+
+let colors = ["Yellow", "Red", "Blue", "Green", "Gray"]
--- BibleReader/Reader/Views/BodyView.swift
+++ BibleReader/Reader/Views/BodyView.swift
@@ -17,9 +17,101 @@ struct BodyView: View {
     @Binding var showContextMenu: Bool
     
     @State var selectedVers: Versek?
-    var colors = ["Yellow", "Red", "Blue", "Green", "Gray"]
-    var ContinuousText: Text {
-        var resultText = Text("")            
+    
+    
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            GeometryReader { geo in
+                VStack {
+                    ScrollView {
+                        VStack {
+                            self.chapterTitle
+                                
+                            if self.viewModel.versek.count > 0 {
+                                VStack(alignment: .leading) {
+                                    if self.settings.reading == .vers {
+                                        self.lineByLineText
+                                    } else {
+                                        self.continuousText
+                                            .onTapGesture {
+                                                withAnimation(.easeInOut) {
+                                                    self.hideBars.toggle()
+                                                }
+                                            }
+                                    }
+                                }
+                                
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                }
+                .frame(width: self.calculateSize(width: geo.size.width))
+
+            }
+                        
+            self.contextMenu(vers: self.selectedVers)
+                .frame(width: 375, height: 65)
+                .background(Color.white)
+                .offset(x: self.showContextMenu ? 0 : UIScreen.main.bounds.width)
+        }
+    }
+    
+    
+}
+
+// MARK: - Subviews
+extension BodyView {
+    var chapterTitle: some View {
+        VStack {
+            Text(self.viewModel.book.name)
+                .font(.custom("Fira Sans Bold", size: 30))
+                .lineSpacing(1.4)
+            Text("\(self.viewModel.chapter). fejezet")
+                .font(.custom("Fira Sans Medium", size: 24))
+                .lineSpacing(1.4)
+                .allowsTightening(true)
+        }
+        .padding(.bottom, 32)
+    }
+    
+    var lineByLineText: some View {
+        ForEach(0..<self.viewModel.versek.count, id:\.self) { index in
+            Group {
+                if self.viewModel.versek[index].szoveg != nil {
+                    Text(self.settings.showVerses ? "\(index + 1) " : "")
+                        .font(self.settings.fontType.value)
+                        .fontWeight(.bold)
+                        + Text(self.viewModel.versek[index].szoveg!.removedHTMLTags)
+                            .font(self.settings.fontType.value)
+ 
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: Alignment.leading)
+            .background(self.getBackgroundColor(vers: self.viewModel.versek[index])?.opacity(0.4))
+            .onTapGesture {
+                withAnimation(.easeInOut) {
+                    self.hideBars.toggle()
+                }
+            }
+            .onLongPressGesture {
+                withAnimation(.easeInOut) {
+                    var vers = self.viewModel.versek[index]
+                    vers.forditas = self.viewModel.translation.rawValue
+                    self.selectedVers = vers
+                    self.showContextMenu.toggle()
+                }
+            }
+            .padding(.vertical, 3)
+            .padding(.horizontal, 16)
+            .lineSpacing(7)
+            .allowsHitTesting(!self.showContextMenu)
+        }
+    }
+    
+    var continuousText: some View {
+        var resultText = Text("")
+            
         for index in viewModel.versek.indices{
             if self.viewModel.versek[index].szoveg != nil {
                 let indexText = Text("\(index + 1) ")
@@ -35,81 +127,60 @@ struct BodyView: View {
             }
         }
         return resultText
-            
+            .padding(.vertical, 3)
+            .padding(.horizontal, 16)
+            .lineSpacing(7)
     }
-    var body: some View {
-        ZStack(alignment: .trailing) {
-            GeometryReader { geo in
-                VStack {
-                    ScrollView {
-                        VStack {
-                            Text(self.viewModel.book.name)
-                                .font(.custom("Fira Sans Bold", size: 30))
-                                .lineSpacing(1.4)
-                            Text("\(self.viewModel.chapter). fejezet")
-                                .font(.custom("Fira Sans Medium", size: 24))
-                                .lineSpacing(1.4)
-                                .padding(.bottom, 32)
-                                .allowsTightening(true)
-                            if self.viewModel.versek.count > 0 {
-                                VStack(alignment: .leading) {
-                                    if self.settings.reading == .vers {
-                                        ForEach(0..<self.viewModel.versek.count, id:\.self) { index in
-                                            Group {
-                                                if self.viewModel.versek[index].szoveg != nil {
-                                                    Text(self.settings.showVerses ? "\(index + 1) " : "")
-                                                        .font(self.settings.fontType.value)
-                                                        .fontWeight(.bold)
-                                                        + Text(self.viewModel.versek[index].szoveg!.removedHTMLTags)
-                                                            .font(self.settings.fontType.value)
-                                                }
-                                            }
-                                            .frame(maxWidth: .infinity, alignment: Alignment.leading)
-                                            .background(self.getBackgroundColor(vers: self.viewModel.versek[index])?.opacity(0.4))
-                                            .onTapGesture {
-                                                withAnimation(.easeInOut) {
-                                                    self.hideBars.toggle()
-                                                }
-                                            }
-                                            .onLongPressGesture {
-                                                withAnimation(.easeInOut) {
-                                                    var vers = self.viewModel.versek[index]
-                                                    vers.forditas = self.viewModel.translation.rawValue
-                                                    self.selectedVers = vers
-                                                    self.showContextMenu.toggle()
-                                                }
-                                            }
-                                            .allowsHitTesting(!self.showContextMenu)
-                                        }
-                                        .padding(.bottom, 4)
-                                        .padding(.horizontal, 20)
-                                        .lineSpacing(9)
-                                    } else {
-                                        VStack {
-                                            self.ContinuousText
-                                                .lineSpacing(9)
-                                                .padding(.bottom, 4)
-                                                .padding(.horizontal, 20)
-                                        }
-                                    }
-                                    
-                                }
-                            }
-                        }
-                        .frame(maxWidth: .infinity)
+    
+    func contextMenu(vers: Versek!) -> some View {
+        VStack {
+            HStack(spacing: 10) {
+                Button(action: {
+                    withAnimation {
+                        self.showContextMenu = false
                     }
-                    
+                }, label: {
+                    Image(systemName: "arrowshape.turn.up.left")
+                        .font(.title)
+                })
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(width: 1, height: 44)
+                
+                ForEach(colors.indices, id:\.self) { index in
+                    Button(action: {
+                        self.viewModel.markVers(vers, index: index)
+                        withAnimation {
+                            self.showContextMenu = false
+                        }
+                    }, label: {
+                        Circle()
+                            .fill(Color(colors[index]))
+                    })
+                    .frame(width: 44)
                 }
-                .frame(width: self.calculateSize(width: geo.size.width))
-
+                
+                Button(action: {
+                    self.viewModel.removeVersMarking(vers)
+                    withAnimation {
+                        self.showContextMenu = false
+                    }
+                }, label: {
+                    ZStack {
+                        Circle()
+                            .stroke(Color.black)
+                        Text("X")
+                    }
+                })
+                .frame(width: 44, height: 44)
             }
-                        
-            ContextMenuView(showContextMenu: self.$showContextMenu, vers: self.selectedVers, viewModel: self.viewModel)
-                .frame(width: 375, height: 65)
-                .background(Color.white)
-                .offset(x: self.showContextMenu ? 0 : UIScreen.main.bounds.width)
         }
+        .padding(4)
     }
+}
+
+// MARK: - Helpers
+extension BodyView {
     
     func calculateSize(width: CGFloat) -> CGFloat {
         if horizontalSizeClass == .regular && verticalSizeClass == .regular {
@@ -142,6 +213,7 @@ struct BodyView: View {
         return attributedString
     }
 }
+
 
 //struct BodyView_Preview: PreviewProvider {
 //
